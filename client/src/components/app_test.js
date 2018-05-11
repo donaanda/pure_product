9160e66049949556cdf8b8a5bbe17eefbb162518
Scott Bowler <scott.bowler@learningfuze.com>
2018-05-11 13:14:45 -0700

    Merged dev from Parent LearningFuze repo dev

---
+++ client/src/components/app_test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../assets/css/app.css';
+import Test from './test';
+
+const App = () => (
+    <div>
+        <div className="app">
+           <Test/>
+        </div>
+    </div>
+);
+
+export default App;
---
+++ client/src/components/footer.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import github from '..//assets/images/footer_icons/git_hub.png';
+import new_user from '../assets/images/footer_icons/new_user.png';
+import our_team from '../assets/images/footer_icons/our_team.png';
+import q_a from '../assets/images/footer_icons/q_a.png';
+import temp_logo from '../assets/images/footer_icons/temp_logo.png';
+
+class Footer extends Component {
+    render() {
+        return (
+            <section className="site-footer">
+                <footer className="site-footer">
+                    <div className="footer-content">
+                        <img src={github} /><span>Github</span>
+                    </div>
+                    <div className="footer-content">
+                        <img src={new_user} /><span>Create Account</span>
+                    </div>
+                    <div className="footer-content">
+                        <img src={our_team} /><span>Our Team</span>
+                    </div>
+                    <div className="footer-content">
+                        <img src={q_a} /><span>FAQ</span>
+                    </div>
+                    <div className="footer-content">
+                        <img src={temp_logo} /><span>About The Site</span>
+                    </div>
+                    <div className="footer-content copyright_span">
+                        <span className="copyright_span">Copyright © 2018  Pure Product . All rights reserved.</span>
+                    </div>
+                </footer>
+            </section>
+        )
+    }
+}
+
+export default Footer;
