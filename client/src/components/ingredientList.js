a62056c8715c1b3320d3fb15fa64b019f427ff3b
Dona Anda <ceka_dona@yahoo.com>
2018-05-13 20:51:02 -0700

    style tabs component

--- client/src/components/ingredientList.js
+++ client/src/components/ingredientList.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import heart from '../assets/images/landing_page_icons/gentle_heart.png';
 import cross from '../assets/images/landing_page_icons/safety.png';
+import '../assets/css/ingredientList.css';
 
-export default ()=>{
+export default () => {
     const chemical = {
         border: '1px solid',
         width: '70%'
@@ -14,12 +15,12 @@ export default ()=>{
         marginTop: '5%'
     }
     const rate = {
-        border: '1px solid',
         width:'10%'
     }
     const img = {
         width: '80%'
     }
+
     return(
         <div>
             <table style={table}>
@@ -31,8 +32,23 @@ export default ()=>{
                     </tr>
                     <tr>
                         <td style={chemical}>water</td>
-                        <td style={rate}>1</td>
-                        <td style={rate}>2</td>
+                        <td className="chemical-gentle-rating" style={rate}>1</td>
+                        <td className="chemical-safety-rating" style={rate}>2</td>
+                    </tr>
+                    <tr>
+                        <td style={chemical}>dimethicone</td>
+                        <td className="chemical-gentle-rating" style={rate}>2</td>
+                        <td className="chemical-safety-rating" style={rate}>2</td>
+                    </tr>
+                    <tr>
+                        <td style={chemical}>talc</td>
+                        <td className="chemical-gentle-rating" style={rate}>5</td>
+                        <td className="chemical-safety-rating" style={rate}>3</td>
+                    </tr>
+                    <tr>
+                        <td style={chemical}>peg-10</td>
+                        <td className="chemical-gentle-rating" style={rate}>5</td>
+                        <td className="chemical-safety-rating" style={rate}>7</td>
                     </tr>
                 </tbody>
             </table>
--- client/src/components/tabs.js
+++ client/src/components/tabs.js
@@ -1,53 +1,55 @@
 import React, {Component} from 'react';
 import IngList from './ingredientList';
 
-class Tabs extends Component{
-    constructor(){
-    super();
-    this.state ={
-        tab: 'ingredients',
-        fieldStyle: {
+class Tabs extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            tab: 'ingredients',
+            fieldStyle: {
+                backgroundColor: '#ffcc99',
+                width:'100vw',
+                height: '50vh',
+                overflow: 'hidden'
+            }
+        }
+
+        this.tabs1 = {
             backgroundColor: '#ffcc99',
-            width:'100vw',
-            height: '50vh',
-            overflow: 'hidden'
+            font: 'bold 20px Arial',
+            color: '#0099ff',
+            display: 'inline-block',
+            width: '33.3vw',
+            cursor: 'pointer',
+            borderColor:'#ffcc99',
+            outline:'none'
+        }
+
+        this.tabs2 = {
+            backgroundColor: '#99ffcc',
+            font: 'bold 20px Arial',
+            color: '#0099ff',
+            display: 'inline-block',
+            width: '33.3vw',
+            cursor: 'pointer',
+            borderColor:'#99ffcc',
+            outline:'none'
+        }
+
+        this.tabs3 = {
+            backgroundColor: '#ffb3ff',
+            font: 'bold 20px Arial',
+            color: '#0099ff',
+            display: 'inline-block',
+            width: '33.3vw',
+            cursor: 'pointer',
+            borderColor:'#ffb3ff',
+            outline:'none'
         }
     }
 
-    this.tabs1 = {
-        backgroundColor: '#ffcc99',
-        font: 'bold 20px Arial',
-        color: '#0099ff',
-        display: 'inline-block',
-        width: '33.3vw',
-        cursor: 'pointer',
-        borderColor:'#ffcc99',
-        outline:'none'
-    }
-
-    this.tabs2 = {
-        backgroundColor: '#99ffcc',
-        font: 'bold 20px Arial',
-        color: '#0099ff',
-        display: 'inline-block',
-        width: '33.3vw',
-        cursor: 'pointer',
-        borderColor:'#99ffcc',
-        outline:'none'
-    }
-
-    this.tabs3 = {
-        backgroundColor: '#ffb3ff',
-        font: 'bold 20px Arial',
-        color: '#0099ff',
-        display: 'inline-block',
-        width: '33.3vw',
-        cursor: 'pointer',
-        borderColor:'#ffb3ff',
-        outline:'none'
-    }
-}
-    handleClickIngredients(event){
+    handleClickIngredients(event) {
         console.log(event.target.name);
         event.preventDefault();
         this.setState({
@@ -61,7 +63,8 @@ class Tabs extends Component{
         });
     
     }
-    handleClickVideos(event){
+
+    handleClickVideos(event) {
         console.log(event.target.name);
         event.preventDefault();
         this.setState({
@@ -75,7 +78,8 @@ class Tabs extends Component{
         });
     
     }
-    handleClickReviews(event){
+
+    handleClickReviews(event) {
         console.log(event.target.name);
         event.preventDefault();
         this.setState({
@@ -87,20 +91,21 @@ class Tabs extends Component{
                 overflow: 'hidden'
             }
         });
-    
     }
 
-    render(){
+    render() {
+
         const {tab} = this.state;
-        return(
-        <div>
-            <button name='ingredients' style={this.tabs1} onClick={this.handleClickIngredients.bind(this)}>Ingredients</button>
-            <button name='videos' style={this.tabs2} onClick={this.handleClickVideos.bind(this)}>Videos</button>
-            <button name='reviews' style={this.tabs3} onClick={this.handleClickReviews.bind(this)}>Reviews</button>
-            <div style={this.state.fieldStyle}>
-                <IngList/>
+
+        return (
+            <div>
+                <button name='ingredients' style={this.tabs1} onClick={this.handleClickIngredients.bind(this)}>Ingredients</button>
+                <button name='videos' style={this.tabs2} onClick={this.handleClickVideos.bind(this)}>Videos</button>
+                <button name='reviews' style={this.tabs3} onClick={this.handleClickReviews.bind(this)}>Reviews</button>
+                <div style={this.state.fieldStyle}>
+                    <IngList/>
+                </div>
             </div>
-        </div>
         )
     }
 }
