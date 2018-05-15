587ffe01c69617df1ddceec3a24219eda570b7f1
Omer <omerfu@gmail.com>
2018-05-15 10:53:02 -0700

    finished file which makes product-ingredient-association-table

--- get_product.php
+++ get_product.php
@@ -1,12 +1,12 @@
 <?php
 require_once('./db_connect.php');
 $search_input = 'Super';
-$query = "SELECT * FROM `product_adding` WHERE `Product_Name` LIKE '%$search_input%'";
+$query = "SELECT * FROM `product_name`";
 //SELECT * FROM `ingredients_rating` where `Ingredient`like '%username%'
 //produce an array
 //feed 1 by 1 through a loop to query -> a result with everything
 //array your going to make is going to start with a number 
-//[1,bucnh of shit]
+//[1,bunch of shit]
 //when inserted array[0], loop through bunch of shit
 //create insert function dynamically 
 $result=mysqli_query($db,$query);
@@ -16,10 +16,9 @@ if(mysqli_num_rows($result)){
     $row=mysqli_fetch_assoc($result);
     $output['success']=true;
     $output['products']=$row; 
-    
+    //console.log($result);
 } else {
-    
     $output['error']='Can\'t find product';
 }
-print(json_encode($output));
+echo 'json encoded: '.json_encode($output);
 ?>
