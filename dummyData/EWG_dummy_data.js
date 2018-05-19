5e6956a5dcb46ef8f8b1f00665a2d97838eaacd2
Alia Wilkinson <aliawilkinson@gmail.com>
2018-05-18 18:05:55 -0700

    added dummy data for EWG

---
+++ dummyData/EWG_dummy_data.js
@@ -0,0 +1,18 @@
+
+
+dimethicone = {
+    name: 'dimethicone',
+    score: 3,
+    data_available: 'limited',
+    concerns: {
+        MODERATE_concerns: 'Organ system toxicity (non-reproductive)',
+        LOW_concerns: 'Data gaps, Ecotoxicology'
+    },
+    about: 'Dimethicone (also called polymethylsiloxane) is a silicon-based polymer used as a lubricant and conditioning agent',
+    functions: 'Antifoaming Agent; Skin-Conditioning Agent - Occlusive; Skin Protectant; EMOLLIENT; SKIN CONDITIONING; SKIN PROTECTING',
+    synonyms: 'DIMETHICONE COPOLYOL; DIMETHYL SILICONE; HIGHLY POLYMERIZED METHYL POLYSILOXANE; METHYL POLYSILOXANE; SILICONE L-45; DC 1664; DIMETHICONE 350; DIMETICONE; DOW CORNING 1664; MIRASIL DM 20; VISCASIL 5M',
+    src: 'https://www.ewg.org/skindeep/ingredient/702011/DIMETHICONE/#.Wv9w31Mvwsk',
+    citation: `"DIMETHICONE" EWG.Accessed May 19, 2018. https://www.ewg.org/skindeep/ingredient/702011/DIMETHICONE/#.Wv9w31Mvwsk.`,
+    hello: this
+}
+
