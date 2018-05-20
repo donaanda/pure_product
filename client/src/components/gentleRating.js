5efc8989f5a105eb72dc2fbd44cf62ef36feab66
Alia Wilkinson <aliawilkinson@gmail.com>
2018-05-20 16:57:38 -0700

    css styling for display products

--- client/src/components/gentleRating.js
+++ client/src/components/gentleRating.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import Header from './header';
 import Footer from './footer';
+import gentle_rating from '../assets/images/landing_page_icons/gentle_heart.png';
 import { Link } from 'react-router-dom';
 
 const GentleRating = () => {
     return (
-        <section className="about_safety">
-            <Header />
+        <section>
+        <Header />
+        <div className="site-about-container">
+        <img className="gentle_icon_gentle_page" src={gentle_rating} />
             <h1>About The Gentle Rating</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed interdum lobortis magna eget elementum. Pellentesque sed viverra eros, ut commodo libero. Curabitur nec neque vitae urna semper sodales. Sed arcu ex, consequat sit amet elit nec, porta sodales lectus. Integer risus nunc, pretium rutrum nisi ac, varius tempus lacus. Sed ultricies nulla urna, ut venenatis felis feugiat eget. Sed ultrices ornare consectetur.
-
-            Aliquam vitae scelerisque turpis. Curabitur nec euismod elit, vel tempor ante. Aenean justo ex, tincidunt vitae sodales eget, porta sed velit. Donec bibendum eleifend vestibulum. Proin lectus nulla, blandit mattis orci id, congue pulvinar ipsum. Aliquam id leo ligula. Aenean vitae erat blandit, hendrerit nisi sit amet, sagittis leo. In consectetur elit tellus, vel vulputate ex viverra malesuada. Pellentesque vel fermentum lorem, vitae pharetra metus. Sed nec urna nec nulla cursus rhoncus sit amet vel mauris. Nunc vulputate vel lorem a finibus.
-
-            Nam in suscipit dui. Donec non fringilla nunc. Nunc finibus pulvinar elementum. Nullam in volutpat turpis, at efficitur ligula. Phasellus eget metus ex. Proin ultrices magna nec ligula bibendum, at faucibus elit mattis. Praesent hendrerit pulvinar turpis, sit amet bibendum velit efficitur sodales. Etiam sodales gravida justo, eget tempus massa elementum in. In maximus non augue ut sodales.
-
-            Sed auctor quis nisl nec sagittis. Duis interdum eleifend est, sodales molestie neque efficitur sit amet. Sed venenatis sed turpis et varius. Aliquam sed finibus velit. Nam lacinia ex nec dolor varius dignissim. Donec mattis ullamcorper libero, id pharetra odio laoreet at. Quisque blandit in nisi et faucibus. Phasellus pellentesque condimentum mi non consequat.
-
-            Aenean ac tortor a leo molestie sodales in ac risus. Praesent rhoncus orci eu est vestibulum, vel laoreet ante accumsan. Curabitur convallis libero non sem scelerisque, quis mollis tellus viverra. Aliquam rhoncus, ante at tempus cursus, mi lectus aliquet neque, nec iaculis diam arcu quis ipsum. Nam imperdiet tortor ut mauris placerat, eget consequat ante ornare. Proin eget nisl hendrerit, tempus nisl vel, elementum ipsum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Duis blandit facilisis sem, vitae mattis augue ullamcorper et. Praesent vitae dui velit. Nam vitae ullamcorper nisl, ac cursus mauris. Sed laoreet eros ante, a fermentum leo venenatis ut. In a placerat ipsum. Nunc pellentesque eu ante ac iaculis. Nullam venenatis ultrices purus, ac mollis tellus. Morbi dui nunc, malesuada pulvinar eros eu, fringilla tempus nibh.
+            <p>Our gentle rating is based on data from Paula's Choice, a site dedicated to determining the dermatological safety of products. This differs from the safety rating in that it determines how gentle products are for a person's skin, as opposed to how safe they are for the average person's body and for the environment at large. For example, parabens are considered "good" under the gentle rating as they cause little or no irritation, while parabens are an 8 under the safety rating, as they can cause endocrine disruption in humans (can be adverse to a person's hormones).</p>
+            <p> All data should be taken with a grain of salt, as the amount of data available on each ingredient can range from robust to limited. Additionally, ever person has different tolerances, allergies, and other limiting factors in which products they can use, and it’s always best to choose products based on your individual needs. We attempt to do our best to organize the data in the most easy to use way possible, but as always, it’s important to consult your doctor when using any new products, as well as listen to your own skin and body when trying something new.</p> 
+            <p>Cheers, 
             </p>
-            <Footer />
+            <p>The Pure Product Team
+            </p>
+
+            
+        </div>
+        <Footer />
         </section>
     )
 }
