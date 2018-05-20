292ad7ac4cd643adf02be98433e6312f7f990085
Alia Wilkinson <aliawilkinson@gmail.com>
2018-05-19 19:06:45 -0700

    fixed css for display product page, changed product display tolerance to 10

--- client/src/components/displayImage.js
+++ client/src/components/displayImage.js
@@ -3,32 +3,57 @@ import '../assets/css/displayImage.css';
 import Image_1 from '../assets/images/displayImages/Image_1.png';
 import Group_1 from '../assets/images/displayImages/Group_1.png';
 import CompoundPath_1 from '../assets/images/displayImages/Compound Path_1.png';
-import Text_1 from '../assets/images/displayImages/Text_1.png';
-import SafetyRating from '../assets/images/displayImages/Text_2.png';
 
 
 export default (props) => {
-    if (props.product === undefined) {
+    if (props.product === undefined || props.product === null) {
         return (<div>no product specified</div>)
-    }
-    console.log(props.product);
+    } else {
+        console.log(props.product);
 
-    const { product_name, brand, price, img_src, categories, Product_ID, gentle_avg_rating, safety_avg_ating} = props.product;
+        const { product_name, brand, price, img_src, categories, Product_ID, gentle_avg_rating, safety_avg_ating } = props.product;
 
-    return (
-        <section>
-            <div className='image-container'>
-                <img className='product-image' src={Image_1} />
-                <img className='gentle-icon' src={CompoundPath_1} />
-                <img className='safety-icon' src={Group_1} />
-                <img className='gentle-rating' src={Text_1} />
-                <img className='safety-rating' src={SafetyRating} />
-                <h1 className='product-name'>Product Name: {product_name}</h1>
-                <h3 className='product-name'>Brand: {brand}</h3>
-                <h3 className='product-name'>Price: {price}</h3>
-                {/* <h3 className='product-name'>Detail: {Detail}</h3> */}
-                <h3 className='product-name'>Categories: {categories}</h3>
-            </div>
-        </section >
-    )
-}
+        return (
+            <div className="product-display-container">
+                <div className='image-container'>
+                    <img className='product-image' src={Image_1} />
+                    <img className='gentle-icon' src={CompoundPath_1} />
+                    <img className='safety-icon' src={Group_1} />
+                    {/* <img className='gentle-rating' src={Text_1} /> */}
+                    {/* <img className='safety-rating' src={SafetyRating} /> */}
+                    <h3 className='product-name'>
+                        <span className="hidden">
+                            Brand:
+                    </span>
+                        <span className="brand-text">
+                            {brand}
+                        </span>
+                    </h3>
+                    <h1 className='product-name'>
+                        <span className="hidden">
+                            Product Name:
+                    </span>
+                        <span className="product-name-text">
+                            {product_name}
+                        </span>
+                    </h1>
+                    <h3 className='product-name'>
+                        <span className="hidden">
+                            Price:
+                    </span>
+                        <span className="price-text">
+                            MSRP: ${price}
+                        </span>
+                    </h3>
+                    {/* <h3 className='product-name'>Detail: {Detail}</h3> */}
+                    <h3 className='product-name hidden'>
+                        {/* <span className="hidden">
+                            Categories:
+                    </span> */}
+                        {categories}
+                    </h3>
+                </div>
+            </div >
+        )
+    };
+};
