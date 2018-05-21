835de87f9cba66cde170500cf31f8580a79179e5
harrison8024 <harrison8024@gmail.com>
2018-05-21 11:56:27 -0700

    added header and json decode in php files

--- get_product_by_categories.php
+++ get_product_by_categories.php
@@ -1,17 +1,21 @@
 <?php
 header("Access-Control-Allow-Origin: *", "Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+$postdata = file_get_contents("php://input"); //to get axios call data
+$request = json_decode($postdata); // decode json
 require_once('./db_connect.php');
 $category = 'foundation';
+$category = addslashes($category);
 $query = "SELECT
-product_name.*,
-`rating`,
-`price`,
-`Cruelty_Free`,
-`Vegan`
-FROM
-`product_name`
-JOIN `product_foundation_table` ON product_name.product_id = product_foundation_table.product_id
-WHERE `categories` LIKE '%$category%' LIMIT 10";
+    product_name.*,
+    `rating`,
+    `price`,
+    `Cruelty_Free`,
+    `Vegan`
+    FROM
+    `product_name`
+    JOIN `product_foundation_table` ON product_name.product_id = product_foundation_table.product_id
+    WHERE `categories` = '$category' LIMIT 10";
 $result=mysqli_query($db,$query);
 
 $output=[
@@ -20,16 +24,21 @@ $output=[
 ];
 $output['success']=false;
 
-if(mysqli_num_rows($result)>0){
-    $row=mysqli_fetch_assoc($result);
-    $output['success']=true;
-    $output['data'][]=$row;
-    while($row=mysqli_fetch_assoc($result)){
+if ($result){
+    if(mysqli_num_rows($result)>0){
+        $row=mysqli_fetch_assoc($result);
+        $output['success']=true;
         $output['data'][]=$row;
-    };
-    
+        while($row=mysqli_fetch_assoc($result)){
+            $output['data'][]=$row;
+        };
+        
+    } else {
+        $output['error']='Can\'t find product';
+    }
 } else {
-    $output['error']='Can\'t find product';
+    error_log(date('Y-m-d H:i:s')." error in query: $query ".mysqli_error($db));
+    $output['error'] = 'Error in query';
 }
 
 print(json_encode($output));
