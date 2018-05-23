770901219301b9735c58c2b849951effe9bcae45
Alia Wilkinson <aliawilkinson@gmail.com>
2018-05-23 10:21:32 -0700

    updated css for product page

--- client/src/components/ingredient.js
+++ client/src/components/ingredient.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import '../assets/css/ingredientList.css';
 
 const Ingredient = (props) => {
 
     return (
-        <tr>
-            <td className="ingredient-column">{props.ingredientInfo.ingredient}</td>
-            <td className="chemical-gentle-rating">{props.ingredientInfo.gentle}</td>
-            <td className="chemical-safety-rating">{props.ingredientInfo.safety}</td>
+        <tr className="ingredient-row">
+            <td className="ingredient-column"><span>{props.ingredientInfo.ingredient}</span></td>
+            <td className="chemical-gentle-rating"><p>{props.ingredientInfo.gentle}</p></td>
+            <td className="chemical-safety-rating"><p>{props.ingredientInfo.safety}</p></td>
         </tr>
     )
 }
