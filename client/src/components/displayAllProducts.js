96506b79dfa491512c04e93dd71e7c6acfcfb3ea
Alia Wilkinson <aliawilkinson@gmail.com>
2018-05-24 11:19:20 -0700

    fixed picture display by moving them into dist

--- client/src/components/displayAllProducts.js
+++ client/src/components/displayAllProducts.js
@@ -7,7 +7,7 @@ const DisplayAllProducts = (props) => {
     console.log(props);
     if (props.data.data === null) {
         return <div>Loading...</div>
-    } else if(!props.data.success){
+    } else if (!props.data.success) {
         return <div>No Result Found</div>
     } else {
         let products = props.data.data.map((item, index) => {
