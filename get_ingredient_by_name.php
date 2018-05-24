7035017023a5ef413dd313175a4926710a5c1c03
harrison8024 <harrison8024@gmail.com>
2018-05-23 17:30:07 -0700

    change query for get ingredient by name

--- get_ingredient_by_name.php
+++ get_ingredient_by_name.php
@@ -1,14 +1,25 @@
 <?php
 require_once('./header.php');
 require_once('./db_connect.php');
-$search_input = 'Water';
+$search_input = 'acai';
 $query = "SELECT 
-`ingredient_name` AS ingredient, 
-`safety_rating` AS safety, 
-`gentle_rating` AS gentle,
-`ingredient_detail` AS details  
-FROM `ingredient_rating`
-WHERE ingredient_name LIKE '%$search_input%' ";
+    a.ingredient_name as ingredient, 
+    a.safety_rating as safety, 
+    CASE WHEN `Rating` = 'Average' 
+    THEN 3 WHEN `Rating` = 'Best' 
+    THEN 1 WHEN `Rating` = 'Good' 
+    THEN 2 WHEN `Rating` = 'Poor' 
+    THEN 4 END AS `gentle`, 
+    COALESCE(details,description) as details 
+    FROM (SELECT 
+        safety_rating, 
+        ingredient_id, 
+        `name to search` as ingredient_name, 
+        `About Info` as details 
+        FROM ewg_data) a 
+    JOIN ingredient_rating 
+    ON ingredient_rating.ingredient_name = a.ingredient_name 
+    WHERE a.ingredient_name LIKE '%$search_input%'";
 
 $result=mysqli_query($db,$query);
 $output=[];
@@ -16,9 +27,10 @@ $output['success']=false;
 if(mysqli_num_rows($result)){
     $row=mysqli_fetch_assoc($result);
     $output['success']=true;
-    $output['products']=$row; 
+    $output['ingredient']=$row; 
     
 } else {
+    error_log(date('Y-m-d H:i:s')." error in query: $query ".mysqli_error($db));
     $output['error']='Can\'t find product';
 }
 print(json_encode($output));
