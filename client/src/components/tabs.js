14e01b7ecdac478320897a6d9af259ae3aa65447
Alia Wilkinson <aliawilkinson@gmail.com>
2018-05-26 13:55:52 -0700

    updated tabs

--- client/src/components/tabs.js
+++ client/src/components/tabs.js
@@ -25,8 +25,6 @@ class MenuTabs extends Component {
         event.preventDefault();
         this.setState({
             tab: event.target.name,
-
-
         });
 
     }
@@ -72,8 +70,12 @@ class MenuTabs extends Component {
     //     $.ajax(youtubeAjaxObject);
     // }
 
+    // onYouTubeIframeAPIReady
+
     async componentDidMount() {
-        await axios.post(`http://s-apis.learningfuze.com/hackathon/youtube/search.php`, {
+        const API_KEY = 'AIzaSyCSizsUkb5GqPfSuxAG43QxyscxxJs7m5E';
+        await axios.get(`https://www.googleapis.com/youtube/v3/search`, {
+            'key': API_KEY,
             'q': 'cosmetics',
             'maxResults': 3,
             'type': 'video',
@@ -115,81 +117,79 @@ class MenuTabs extends Component {
     render() {
         //console.log(this.product);
         const { Details, Dry, Normal, Oily, Sensitive, Vegan } = this.product;
+        const { tab } = this.state;
 
-        render() {
-            const { tab } = this.state;
-
-            return (
-                <Tabs selectedIndex={this.state.tabIndex} onSelect={tabIndex => this.setState({ tabIndex })}>
-                    <TabList className="tab-list">
-                        <Tab className="tab-header-ingredient" onClick={this.handleClickIngredients.bind(this)}>
-                            Ingredients
+        return (
+            <Tabs selectedIndex={this.state.tabIndex} onSelect={tabIndex => this.setState({ tabIndex })}>
+                <TabList className="tab-list">
+                    <Tab className="tab-header-ingredient" onClick={this.handleClickIngredients.bind(this)}>
+                        Ingredients
                     </Tab>
-                        <Tab className="tab-header-detail" onClick={this.handleClickDetails.bind(this)}>
-                            Details
+                    <Tab className="tab-header-detail" onClick={this.handleClickDetails.bind(this)}>
+                        Details
                     </Tab>
-                        <Tab className="tab-header-video" onClick={this.handleClickVideos.bind(this)}>
-                            Videos
+                    <Tab className="tab-header-video" onClick={this.handleClickVideos.bind(this)}>
+                        Videos
                     </Tab>
-                        <Tab className="tab-header-review" onClick={this.handleClickReviews.bind(this)}>
-                            Reviews
+                    <Tab className="tab-header-review" onClick={this.handleClickReviews.bind(this)}>
+                        Reviews
                     </Tab>
-                    </TabList>
-                    <TabPanel>
-                        <div className="tab-ingredient">
-                            <IngredientList info={this.ingredients} />
-                        </div>
-                    </TabPanel>
-                    <TabPanel>
-                        <div className="tab-detail">
-                            <p className="product-detail-description">{Details}</p>
-                            <table className="product-detail-table">
-                                <tbody>
-                                    <tr className="product-detail-row">
-                                        <td>Dry</td>
-                                        <td className="product-detail-yes"><p className={`${this.colorizeYes(Dry)}`}>Yes</p></td>
-                                        <td className="product-detail-No"><p className={`${this.colorizeNo(Dry)}`}>No</p></td>
-                                    </tr>
-                                    <tr className="product-detail-row">
-                                        <td>Normal</td>
-                                        <td className="product-detail-yes"><p className={`${this.colorizeYes(Normal)}`}>Yes</p></td>
-                                        <td className="product-detail-No"><p className={`${this.colorizeNo(Normal)}`}>No</p></td>
-                                    </tr>
-                                    <tr className="product-detail-row">
-                                        <td>Oily</td>
-                                        <td className="product-detail-yes"><p className={`${this.colorizeYes(Oily)}`}>Yes</p></td>
-                                        <td className="product-detail-No"><p className={`${this.colorizeNo(Oily)}`}>No</p></td>
-                                    </tr>
-                                    <tr className="product-detail-row">
-                                        <td>Sensitive</td>
-                                        <td className="product-detail-yes"><p className={`${this.colorizeYes(Sensitive)}`}>Yes</p></td>
-                                        <td className="product-detail-No"><p className={`${this.colorizeNo(Sensitive)}`}>No</p></td>
-                                    </tr>
-                                    <tr className="product-detail-row">
-                                        <td>Vegan</td>
-                                        <td className="product-detail-yes"><p className={`${this.colorizeYes(Vegan)}`}>Yes</p></td>
-                                        <td className="product-detail-No"><p className={`${this.colorizeNo(Vegan)}`}>No</p></td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </TabPanel>
-                    <TabPanel>
-                        <div className="tab-video">
-                            <h2 className="videos-default-text">No videos to display.</h2>
-                        </div>
-                    </TabPanel>
-                    <TabPanel>
-                        <div className="tab-review">
-                            <h2>No Reviews Yet</h2>
-                            <h5>Be the first to write a review!</h5>
-                            <button className="tab-review-button">Add Review</button>
-                        </div>
-                    </TabPanel>
-                </Tabs>
-            )
-        }
+                </TabList>
+                <TabPanel>
+                    <div className="tab-ingredient">
+                        <IngredientList info={this.ingredients} />
+                    </div>
+                </TabPanel>
+                <TabPanel>
+                    <div className="tab-detail">
+                        <p className="product-detail-description">{Details}</p>
+                        <table className="product-detail-table">
+                            <tbody>
+                                <tr className="product-detail-row">
+                                    <td>Dry</td>
+                                    <td className="product-detail-yes"><p className={`${this.colorizeYes(Dry)}`}>Yes</p></td>
+                                    <td className="product-detail-No"><p className={`${this.colorizeNo(Dry)}`}>No</p></td>
+                                </tr>
+                                <tr className="product-detail-row">
+                                    <td>Normal</td>
+                                    <td className="product-detail-yes"><p className={`${this.colorizeYes(Normal)}`}>Yes</p></td>
+                                    <td className="product-detail-No"><p className={`${this.colorizeNo(Normal)}`}>No</p></td>
+                                </tr>
+                                <tr className="product-detail-row">
+                                    <td>Oily</td>
+                                    <td className="product-detail-yes"><p className={`${this.colorizeYes(Oily)}`}>Yes</p></td>
+                                    <td className="product-detail-No"><p className={`${this.colorizeNo(Oily)}`}>No</p></td>
+                                </tr>
+                                <tr className="product-detail-row">
+                                    <td>Sensitive</td>
+                                    <td className="product-detail-yes"><p className={`${this.colorizeYes(Sensitive)}`}>Yes</p></td>
+                                    <td className="product-detail-No"><p className={`${this.colorizeNo(Sensitive)}`}>No</p></td>
+                                </tr>
+                                <tr className="product-detail-row">
+                                    <td>Vegan</td>
+                                    <td className="product-detail-yes"><p className={`${this.colorizeYes(Vegan)}`}>Yes</p></td>
+                                    <td className="product-detail-No"><p className={`${this.colorizeNo(Vegan)}`}>No</p></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </TabPanel>
+                <TabPanel>
+                    <div className="tab-video">
+                        <h2 className="videos-default-text">No videos to display.</h2>
+                    </div>
+                </TabPanel>
+                <TabPanel>
+                    <div className="tab-review">
+                        <h2>No Reviews Yet</h2>
+                        <h5>Be the first to write a review!</h5>
+                        <button className="tab-review-button">Add Review</button>
+                    </div>
+                </TabPanel>
+            </Tabs>
+        )
     }
+}
 
 
-    export default MenuTabs;
+export default MenuTabs;
