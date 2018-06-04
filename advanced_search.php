5ce2365505cc49668b89540d7c05b77b55d93c3e
Alia Wilkinson <aliawilkinson@gmail.com>
2018-06-04 14:06:47 -0700

    deleted files

--- advanced_search.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('./header.php');
-$postdata = file_get_contents("php://input"); //to get axios call data
-$request = json_decode($postdata, true); // decode json
-require_once('./db_connect.php');
-$advanced_search_ob = $request['query'];
-print_r($advanced_search_ob); 
-?>
-
