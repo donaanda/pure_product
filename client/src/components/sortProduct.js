d13e38016847ea4c4e448b055b42a4221f3613dc
Dona Anda <ceka_dona@yahoo.com>
2018-06-07 20:00:52 -0700

    added sort product by category

---
+++ client/src/components/sortProduct.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import '../assets/css/sortProduct.css';
+
+class SortProduct extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            dropContent: 'hide-drop-content',
+            dropDownMenuClicked: false
+        };
+        this.dropDownMenu = this.dropDownMenu.bind(this);
+    }
+
+    dropDownMenu() {
+        if (this.state.dropDownMenuClicked) {
+            this.setState({
+                dropContent: 'hide-drop-content',
+                dropDownMenuClicked: false
+            });
+        } else {
+            this.setState({
+                dropContent: 'show-drop-content',
+                dropDownMenuClicked: true
+            });
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <button className="drop-down" onClick={this.dropDownMenu}>Sort</button>
+                <div className={this.state.dropContent}>
+                    <div className="drop-down-category" onClick={this.props.selectedCategory}>Price</div>
+                    <div className="drop-down-category" onClick={this.props.selectedCategory}>Gentle Rating</div>
+                    <div className="drop-down-category" onClick={this.props.selectedCategory}>Safety Rating</div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default SortProduct;
